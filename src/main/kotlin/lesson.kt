751562c7cdb4d466e0a06f7555e55b82aa0b70cd
hide0621 <fujihara_hideyuki@retail-ai.jp>
2023-02-11 11:00:40 +0900

    画面出力・四則演算・文字列結合まで終了

--- src/main/kotlin/lesson.kt
+++ src/main/kotlin/lesson.kt
@@ -1,3 +1,14 @@
 fun main() {
-    print(100)
+//    println(100)
+//    println("Hello")
+//
+//    print("Hello\nworld")
+
+//    println(10 + 2)
+//    println(10 - 2)
+//    println(10 * 2)
+//    println(10 / 2)
+//    println(10 % 2)
+
+    println("Hello" + "world")
 }
