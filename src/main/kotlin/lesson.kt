f822e31904f5412a24b2ba7afe594c1274434266
hide0621 <fujihara_hideyuki@retail-ai.jp>
2023-02-13 10:08:04 +0900

    リストまで終了

--- src/main/kotlin/lesson.kt
+++ src/main/kotlin/lesson.kt
@@ -1,12 +1,24 @@
 fun main() {
-    val array : Array<Int> = arrayOf(10, 20, 30)
-//    println(array[0])
-//    array[0] = 100
-//    println(array[0])
-    array.forEach { println(it) } // ラムダ式
 
-    val array2 : IntArray = intArrayOf(10, 20)
+    val list: MutableList<Int> = mutableListOf(10, 20, 30)
+    println(list[0])
 
-    val array3: Array<String?> = arrayOfNulls<String>(5)
-    array3.forEach { println(it) } // ラムダ式
+    list[0] = 100
+
+    println(list[0])
+
+    println(list)
+
+    list.add(200)
+    println(list)
+
+    // インデックス番号０に追加（上書きではない）
+    list.add(0, 500)
+    println(list)
+
+    list.removeAt(0)
+    println(list)
+
+    list.remove(100)
+    println(list)
 }
