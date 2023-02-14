f873fc8707c521849593366d868006d8afc788ef
hide0621 <fujihara_hideyuki@retail-ai.jp>
2023-02-14 13:19:42 +0900

    ジャンプ演算子break,continueの学習

--- src/main/kotlin/lesson.kt
+++ src/main/kotlin/lesson.kt
@@ -1,32 +1,37 @@
 fun main() {
 
-//    val array = arrayOf("りんご", "みかん", "ぶどう")
-//
-//    for(fruits in array) {
-//        println(fruits)
+//    for(i in 0..10 ) {
+//        if (i == 5) {
+//            break
+//        }
+//        println(i)
 //    }
 
-//    val list = listOf("東京", "大阪", "福岡")
-//    for ((index, place) in list.withIndex()) {
-//        println("$index = $place")
+//    for(i in 0..10 ) {
+//        if (i%2 == 0) {
+//            continue
+//        }
+//        println(i)
 //    }
 
-//    for (index in list.indices) {
-//        println(index)
-//        println(list[index])
+//    for (i in 0..10) {
+//        for (j in 0..10) {
+//            if (j == 5) {
+//                break
+//            }
+//            print(j)
+//        }
+//        println()
 //    }
 
-    val map = mapOf<Int, String>(
-        1 to "one",
-        2 to "two",
-        3 to "three"
-    )
-
-//    for(data in map) {
-//        println(data)
-//    }
-    for((key, value) in map) {
-        println("$key = $value")
+    outer@for (i in 0..10) {
+        for (j in 0..10) {
+            if (j == 5) {
+                break@outer
+            }
+            print(j)
+        }
+        println()
     }
 
 }
