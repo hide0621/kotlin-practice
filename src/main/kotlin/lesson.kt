4fdecbb7ea3207dd86a358c38717a0e1ed77025a
hide0621 <fujihara_hideyuki@retail-ai.jp>
2023-02-14 10:43:17 +0900

    when文まで学習

--- src/main/kotlin/lesson.kt
+++ src/main/kotlin/lesson.kt
@@ -1,23 +1,36 @@
 fun main() {
 
-    val num: Int = 49
+    val num: Any = 30.0
 
-//    if (num > 100) {
-//        println("num > 100")
-//    } else if (num > 50) {
-//        println("num > 50")
-//    } else {
-//        println("その他")
+//    when(num) {
+//        5 -> println("num = 5")
+//        6,7 -> println("num = 6 or 7")
+//        in 11..20 -> println("num = 11 ~ 20")
+//        else -> {
+//            println("その他")
+//            println("当てはまるものなし！")
+//        }
 //    }
 
-    val str: String = if (num > 100) {
-        "num > 100"
-    } else if (num > 50) {
-        "num > 50"
-    } else {
-        "その他"
+//    val str: String = when(num) {
+//        5 -> "num = 5"
+//        6,7 -> "num = 6 or 7"
+//        in 11..20 -> "num = 11 ~ 20"
+//        else -> {
+//            println("その他")
+//            "当てはまるものなし！"
+//        }
+//    }
+//
+//    println(str)
+
+    val str: Any = when(num) {
+        is Int -> "Int"
+        is Double -> "Double"
+        is String -> "String"
+        else -> {
+            println("当てはまるものなし！")
+        }
     }
-
-    println("strは${str}です")
-
+    println(str)
 }
