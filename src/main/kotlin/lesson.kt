2f7e3bc54f942f985d91ea5fa5ecf430989626b4
hide0621 <fujihara_hideyuki@retail-ai.jp>
2023-02-14 14:31:24 +0900

    デフォルト引数、名前付き引数の学習

--- src/main/kotlin/lesson.kt
+++ src/main/kotlin/lesson.kt
@@ -1,18 +1,8 @@
 fun main() {
-//    println(union(1, 2, 3, 4))
-
-    var intArray = intArrayOf(1, 2, 3, 4)
-    // スプレッド演算子（*intArray）
-    println(union(1, 2, 3, 4))
+//    val num = calcSquareArea(5)
+    val num = calcSquareArea(width = 10)
+    println(num)
 }
 
-fun union(vararg array : Int): String {
-
-    var str = ""
-    for (num in array) {
-        str += num
-    }
-
-    return str
-}
-
+//fun calcSquareArea(height: Int, width: Int  = 5 ) = height * width
+fun calcSquareArea(height: Int = 5 , width: Int) = height * width
