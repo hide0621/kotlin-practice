31f347a2e92e80bad0e5f542756f168245a37e6c
hide0621 <fujihara_hideyuki@retail-ai.jp>
2023-02-14 13:04:40 +0900

    for文と配列、リスト、マップの組み合わせを学習

--- src/main/kotlin/lesson.kt
+++ src/main/kotlin/lesson.kt
@@ -1,26 +1,32 @@
 fun main() {
 
-//    for(i in 0..10) {
-//        println(i)
+//    val array = arrayOf("りんご", "みかん", "ぶどう")
+//
+//    for(fruits in array) {
+//        println(fruits)
 //    }
 
-//    for(i in 0 until  10) {
-//        println(i)
+//    val list = listOf("東京", "大阪", "福岡")
+//    for ((index, place) in list.withIndex()) {
+//        println("$index = $place")
 //    }
 
-//    for(i in 10 downTo 0) {
-//        println(i)
+//    for (index in list.indices) {
+//        println(index)
+//        println(list[index])
 //    }
 
-//    for(i in 10 downTo 0 step 2) {
-//        println(i)
-//    }
+    val map = mapOf<Int, String>(
+        1 to "one",
+        2 to "two",
+        3 to "three"
+    )
 
-    for(i in 1..9) {
-        for(j in 1..9) {
-            print(i * j)
-        }
-        println()
+//    for(data in map) {
+//        println(data)
+//    }
+    for((key, value) in map) {
+        println("$key = $value")
     }
 
 }
