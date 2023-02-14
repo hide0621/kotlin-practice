404bb44f3e66442ac4fd61da58bb1caf712068af
hide0621 <fujihara_hideyuki@retail-ai.jp>
2023-02-14 11:06:38 +0900

    while文とdo-while文の学習

--- src/main/kotlin/lesson.kt
+++ src/main/kotlin/lesson.kt
@@ -1,36 +1,15 @@
 fun main() {
 
-    val num: Any = 30.0
-
-//    when(num) {
-//        5 -> println("num = 5")
-//        6,7 -> println("num = 6 or 7")
-//        in 11..20 -> println("num = 11 ~ 20")
-//        else -> {
-//            println("その他")
-//            println("当てはまるものなし！")
-//        }
-//    }
-
-//    val str: String = when(num) {
-//        5 -> "num = 5"
-//        6,7 -> "num = 6 or 7"
-//        in 11..20 -> "num = 11 ~ 20"
-//        else -> {
-//            println("その他")
-//            "当てはまるものなし！"
-//        }
-//    }
-//
-//    println(str)
-
-    val str: Any = when(num) {
-        is Int -> "Int"
-        is Double -> "Double"
-        is String -> "String"
-        else -> {
-            println("当てはまるものなし！")
-        }
+    var i = 10
+    while (i < 10) {
+        println("i = $i")
+        i++
     }
-    println(str)
+
+    var j = 10
+    do {
+        println("j = $j")
+        j++
+    } while (j < 10)
+
 }
