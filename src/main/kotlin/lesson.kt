ae716b234cbdf6c742885d56d315d668ee1374ec
hide0621 <fujihara_hideyuki@retail-ai.jp>
2023-02-15 09:39:13 +0900

    オブジェクト指向の基礎まで学習

--- src/main/kotlin/lesson.kt
+++ src/main/kotlin/lesson.kt
@@ -1,33 +1,16 @@
-import kotlin.reflect.KFunction2
 
 fun main() {
 
-// ラムダ式の場合
-
-//    val func: KFunction2<Int, Int, Int> = ::calcA
-//    println(func(10, 5))
-//    val n1 = calcB(5, 10, ::calcA)
-//    println("n1 = $n1")
-
-//    val n2 = calcB(5, 10, {x: Int, y: Int -> x + y})
-//    val n2 = calcB(5, 10){x: Int, y: Int -> x + y}
-//    println("n2 = $n2")
-
-//    val num = funcB(10, ::funcA)
-//    val num = funcB(10) { x: Int -> x + 1 }
-//    val num = funcB(10){it + 1}
-//    println(num)
-
-//　無名関数の場合
-    val num = calcB(5, 10, fun (x: Int, y: Int):Int = x + y)
-    println(num)
-
+    val player = Character()
+    player.showStatus()
 }
 
-//fun calcA(a: Int, b: Int): Int = a + b
+class Character {
+    var name: String = "主人公"
+    var hp: Int = 100
 
-fun calcB(a: Int, b: Int, func: (Int, Int) -> Int) = func(a, b)
-
-//fun funcA(a: Int) = a + 1
-//
-//fun funcB(a: Int, func: (Int) -> Int) = func(a)
+    fun showStatus() {
+        println("名前:${name}")
+        println("HP:${hp}")
+    }
+}
