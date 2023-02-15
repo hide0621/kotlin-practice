7c1d77f116b92aaa8a803c891bc94189b666e850
hide0621 <fujihara_hideyuki@retail-ai.jp>
2023-02-15 10:25:27 +0900

    セカンダリコンストラクタの学習

--- src/main/kotlin/lesson.kt
+++ src/main/kotlin/lesson.kt
@@ -4,7 +4,13 @@ fun main() {
     val p1 = ch("プレイヤー1", 100)
     p1.showStatus()
 
-    val p2 = ch("プレイヤー2", 200)
+    val p2 = ch("プレイヤー2")
     p2.showStatus()
+
+    val p3 = ch(500)
+    p3.showStatus()
+
+    val p4 = ch()
+    p4.showStatus()
 }
 
--- src/main/kotlin/model/Character.kt
+++ src/main/kotlin/model/Character.kt
@@ -2,6 +2,14 @@ package model
 
 class Character(var name: String,  var hp: Int) {
 
+    constructor(name: String): this(name, 50) {
+        println("** セカンダリ **")
+    }
+
+    constructor(hp: Int): this("プレイヤー3", hp)
+
+    constructor(): this("プレイヤー4", 10)
+
     fun showStatus() {
         println("名前:${name}")
         println("HP:${hp}")
